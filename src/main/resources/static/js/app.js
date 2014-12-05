6b972f206eb67ac277519b36f2da70dabc31ffa0
Sean Livingston <sean.d.livingston@gmail.com>
2014-12-05 00:28:27 -0500

    builds out controller.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -38,3 +38,79 @@ angular.module('studentsApp')
 
         return studentFactory;
     }]);
+
+
+angular.module('studentsApp')
+    .controller('studentsController', ['$scope', 'studentFactory', function($scope, studentFactory) {
+
+        $scope.status;
+        $scope.students;
+
+        getStudents();
+
+        function getStudents() {
+            studentFactory.getStudents()
+                .success(function (students) {
+                    $scope.students = students;
+                })
+                .error(function (error) {
+                    $scope.status = 'Unable to load students ' + error.message
+                });
+        }
+
+        $scope.updateStudent = function(id) {
+            var student;
+            for(var i = 0; i < $scope.students.length; i++) {
+                var currentStudent = $scope.students[i];
+                if (currentStudent.id === id) {
+                    student = currentStudent;
+                    break;
+                }
+            }
+
+            studentFactory.updateStudent(student)
+                .success(function () {
+                    $scope.status = 'Student Updated!  Refreshing List';
+                })
+                .error(function (error) {
+                    $scope.status = 'Unable to update Student ' + error.message;
+                });
+        };
+
+        $scope.addStudent = function() {
+            //Fake Student Data
+            var student = {
+                id: 10,
+                firstName: 'JoJo',
+                lastName: 'AJ'
+            };
+
+            studentFactory.addStudent(student)
+                .success(function () {
+                    $scope.status = 'Student Added!  Refreshing student list';
+                })
+                .error(function(error) {
+                    $scope.status = 'Unable to add student: ' + error.message;
+                });
+        };
+
+        $scope.deleteStudent = function(id) {
+            studentFactory.deleteStudent(id)
+                .success(function() {
+                    $scope.status = 'Student Deleted!  Refreshing student list';
+                    for(var i = 0; i < $scope.students.length; i++) {
+                        var student = $scope.students[i];
+                        if(student.id === id) {
+                            $scope.students.splice(i, 1);
+                            break;
+                        }
+                    }
+                })
+                .error(function(error) {
+                    $scope.status = 'Unable to delete student: ' + error.message;
+                });
+        };
+
+
+    }
+    ]);
